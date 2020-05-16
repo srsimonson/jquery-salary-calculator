4f2be810b4e9b59e7c1bafdfffd966db900cec08
Steve Simonson <srsimonson@gmail.com>
2020-05-15 23:53:43 -0500

    html css basic structure complete

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('js');
+$( document ).ready( canThisBeTaco );
+function canThisBeTaco(){
+    console.log('jq');
+    // functions get called here
+} // end readyNow
+
+//functions get created here
