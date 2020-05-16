a2a4c9f2e7620e9fd9c701ddaf181157588a9b47
Steve Simonson <srsimonson@gmail.com>
2020-05-16 18:08:20 -0500

    table still not cooperating

--- script.js
+++ script.js
@@ -1,51 +1,40 @@
-console.log('js');
 $( document ).ready( readyNow );
 function readyNow(){
-    console.log('jq');
-  
-    // functions get called here
-    addNumbers(3, 2);
+    // functions get called here:
     $('#submitButton').on('click', handleSubmit);
+
 
 } // end readyNow
 
-
-
 // functions get created here
-function addNumbers (num1, num2){
-    console.log(num1 + num2);
-    return num1 + num2;
-}
-
-let employeeArray = [];
-function handleSubmit() {
-    let firstName = $( '#firstName' ).val();
+let employeeArray = []; // empty array of employees to be pushed
+function handleSubmit() { // what happens when you click submit
+    let firstName = $( '.firstName' ).val(); // retrieve data from DOM input
     let lastName = $( '.lastName' ).val();
-    let idNumber = $( '#idNumber' ).val();
-    let title = $( '#title' ).val();
-    let annualSalary = $( '#annualSalary' ).val();
-    let employeeObject = {
+    let idNumber = $( '.idNumber' ).val();
+    let title = $( '.title' ).val();
+    let annualSalary = $( '.annualSalary' ).val();
+    let employeeObject = { // empty employee object
         firstName: firstName,
         lastName: lastName,
         idNumber: idNumber,
         title: title,
         annualSalary: annualSalary
-    } // end personObject
-    employeeArray.push( employeeObject );
-    $( '#firstName' ).val('');
+    } // end employeeObject
+    employeeArray.push( employeeObject ); // push employee object into employee array
+    $( '.firstName' ).val(''); // clear out name values
     $( '.lastName' ).val('');
-    $( '#idNumber' ).val('');
-    $( '#title' ).val('');
-    $( '#annualSalary' ).val('');
-    displayEmployees( employeeArray );
+    $( '.idNumber' ).val('');
+    $( '.title' ).val('');
+    $( '.annualSalary' ).val('');
+    displayEmployeeTable ( employeeArray ); // not running?
     console.log('employeeArray:', employeeArray);
-    
 } // end handleSubmit function
 
-function displayEmployees( arrayParam ) {
-    $( '#peopleList').empty();
+function displayEmployeeTable( arrayParam ) {
+    // $( '#employeeTable').empty();
     for ( let i=0; i<arrayParam.length; i++ )
-    $('#peopleList').append(`
+    $('employeeTable').append(`
             <td>${arrayParam[i].firstName}</td>
             <td>${arrayParam[i].lastName}</td>
             <td>${arrayParam[i].idNumber}</td>
@@ -53,3 +42,51 @@ function displayEmployees( arrayParam ) {
             <td>${arrayParam[i].annualSalary}</td>
         `);
 }
+
+
+// function displayEmployeeTable ( arrayParam ) {
+//     $( '#employeeTable' ).empty();
+//     $( '#submitButton').on('click', newRow)
+
+// } // end displayEmployeeTable function
+
+// function newRow () {
+//     // let first = $( '.firstName' ).val('');
+//     // let last = $( '.lastName' ).val('');
+//     // let idNumber = $( '.idNumber' ).val('');
+//     // let title = $( '.title' ).val('');
+//     // let annualSalary = $( '.annualSalary' ).val('');
+//     $( '#peopleList').empty();
+//     for ( let i=0; i<arrayParam.length; i++ )
+//     // $('#peopleList').append(`
+//     //         <td>${arrayParam[i].firstName}</td>
+//     //         <td>${arrayParam[i].lastName}</td>
+//     //         <td>${arrayParam[i].idNumber}</td>
+//     //         <td>${arrayParam[i].title}</td>
+//     //         <td>${arrayParam[i].annualSalary}</td>
+//     //     `);
+//     let markup = `
+//     <tr>
+//         <td>${arrayParam[i].firstName}</td>
+//         <td>w</td>
+//         <td>e</td>
+//         <td>r</td>
+//         <td>t</td>
+//         <td><button class="deleteBtn">delete</button></td>
+//     </tr>`;
+//     $("table tbody").append(markup);
+//     // or let button = `<li>${arrayParam[i].firstName} ${arrayParam[i].lastName}<button class="deleteBtn">delete<button></li>`
+// } // end newRow function
+
+
+// const row = tr td. append(row)
+// delete row
+// function deleterow is a loop
+
+// let row = (<th>
+//     <td>${arrayParam[i].firstName}</td>
+//     <td>${arrayParam[i].lastName}</td>
+//     <td>${arrayParam[i].idNumber}</td>
+//     <td>${arrayParam[i].title}</td>
+//     <td>${arrayParam[i].annualSalary}</td>
+// </th>);
