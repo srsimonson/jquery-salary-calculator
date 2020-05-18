cbf16299033348c84bfcc369d94986445bf6aff2
Steve Simonson <srsimonson@gmail.com>
2020-05-17 23:18:47 -0500

    basic table to DOM working

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $( document ).ready( readyNow );
 function readyNow(){
-    // functions get called here:
+// functions get called here:
     $('#submitButton').on('click', handleSubmit);
-    $('#employeeList').on('deleteBtn').on('click', '.deleteBtn', deleteItem);
+    $('#employeeTable').on('deleteBtn').on('click', '.deleteBtn', deleteItem);
     //$('#footer').on('something', turnRed);
 
 } // end readyNow
 
-// functions get created here
+// functions get created here:
 const totalBudget = 20000;
 let monthlySpending = 0;
 let employeeArray = []; // empty array of employees to be pushed
@@ -33,135 +33,61 @@ function handleSubmit() { // what happens when you click submit
     $( '.title' ).val('');
     $( '.annualSalary' ).val('');
     displayToDom( employeeArray );
-    // tallyMonthlyExpenses( monthlyExpenses );
-    // displayEmployeeTable ( employeeArray );
-    
 } // end handleSubmit function
 
 function deleteItem() {
-    $(this).parent().remove();
+    $(this).parent().parent().remove();
 }
 
 function displayToDom() {
-    //turnRed();
     let x = 0;
     let monthlyLabor = 0;
-    $('#employeeList').empty();
+    $('#employeeTable').empty();
     $('#totalSalaries').empty();
     for (let i=0; i<employeeArray.length; i++) {
-        monthlyLabor = monthlyLabor + Number((employeeArray[i].annualSalary / 12));
-        $('#employeeList').append(
-        `<li>
-            ${employeeArray[i].firstName}
-            ${employeeArray[i].lastName}
-            ${employeeArray[i].idNumber}
-            ${employeeArray[i].title}
-            ${employeeArray[i].annualSalary} 
-            <button class="deleteBtn">delete</button>
-        </li>`)
+        monthlyLabor =  monthlyLabor + Number((employeeArray[i].annualSalary / 12));
+        $('#employeeTable').append(
+        // `<li>
+        //     ${employeeArray[i].firstName}
+        //     ${employeeArray[i].lastName}
+        //     ${employeeArray[i].idNumber}
+        //     ${employeeArray[i].title}
+        //     ${employeeArray[i].annualSalary} 
+        //     <button class="deleteBtn">delete</button>
+        `<tr>
+            <td>${employeeArray[i].firstName}</td>
+             <td>${employeeArray[i].lastName}</td>
+             <td>${employeeArray[i].idNumber}</td>
+             <td>${employeeArray[i].title}</td>
+             <td>${employeeArray[i].annualSalary}</td> 
+             <td><button class="deleteBtn">delete</button></td>
+         </td>`)
+
+
+
+
+
+
+
+
+        // </li>`)
     } // end for loop
     x = x + monthlyLabor;
     monthlySpending = x;
     $('#totalSalaries').empty();
-    $('#totalSalaries').append(x);
+    $('#totalSalaries').append(x.toFixed(2));
     console.log('in turnRed: ', monthlySpending);
     if ( monthlySpending > totalBudget) {
         $('#totalSalaries').css({"background-color":"red"});
     }
 } // end displayToDom
 
-// function turnRed(){
-//     console.log('in turnRed: ', monthlySpending);
-//     if ( monthlySpending > totalBudget) {
-//         $('#totalSalaries').css({"background-color":"red"});
-//     }
-// }
-
-// function appendEmployee() {
-//     $('#iAppend').empty();
-//     annualLabor = 0;
-//     for (let i = 0; i < employeesEntered.length; i++) {
-//         annualLabor += Number(employeesEntered[i].annualSalaryEntry);
-//         // console.log('annualLabor', annualLabor); //Functions as intended
-//         $('#iAppend').append(`
-//         <tr>  
-//             <td>${employeesEntered[i].firstNameEntry}</td>
-//             <td>${employeesEntered[i].lastNameEntry}</td>
-//             <td>${employeesEntered[i].iDEntry}</td>  
-//             <td>${employeesEntered[i].jobTitleEntry}</td>
-//             <td>${employeesEntered[i].annualSalaryEntry}</td>
-//             <td><button class = "deleteBtn">Delete</button></td> 
-//APPENDS our variables ^ to their respective fields
-//         </tr>
-//Also creates a delete button to be displayed inline with other appendages. :]
-    
-//         `);
-//     }
-//     calcMonthlyCost(); //calling our new function calcMonthlyCost which we will use to help us put an inline "counter" on monthlyLabor
-// }
-
-
-
-
-// function displayEmployeeTable( arrayParam ) {
-//     // $( '#employeeTable').empty();
-//     for ( let i=0; i<arrayParam.length; i++ )
-//     console.log('in displayEmployeeTable:');
-//     $('employeeTable').append();
-// }
-
-
-// function displayEmployeeTable ( arrayParam ) {
-//     $( '#employeeTable' ).empty();
-//     $( '#submitButton').on('click', newRow)
-
-// } // end displayEmployeeTable function
-
-// function newRow () {
-//     // let first = $( '.firstName' ).val('');
-//     // let last = $( '.lastName' ).val('');
-//     // let idNumber = $( '.idNumber' ).val('');
-//     // let title = $( '.title' ).val('');
-//     // let annualSalary = $( '.annualSalary' ).val('');
-//     $( '#peopleList').empty();
-//     for ( let i=0; i<arrayParam.length; i++ )
-//     // $('#peopleList').append(`
-//     //         <td>${arrayParam[i].firstName}</td>
-//     //         <td>${arrayParam[i].lastName}</td>
-//     //         <td>${arrayParam[i].idNumber}</td>
-//     //         <td>${arrayParam[i].title}</td>
-//     //         <td>${arrayParam[i].annualSalary}</td>
-//     //     `);
-//     let markup = `
-//     <tr>
-//         <td>${arrayParam[i].firstName}</td>
-//         <td>w</td>
-//         <td>e</td>
-//         <td>r</td>
-//         <td>t</td>
-//         <td><button class="deleteBtn">delete</button></td>
-//     </tr>`;
-//     $("table tbody").append(markup);
-//     // or let button = `<li>${arrayParam[i].firstName} ${arrayParam[i].lastName}<button class="deleteBtn">delete<button></li>`
-// } // end newRow function
-
-
-// const row = tr td. append(row)
-// delete row
-// function deleterow is a loop
-
-// let row = (<th>
-//     <td>${arrayParam[i].firstName}</td>
-//     <td>${arrayParam[i].lastName}</td>
-//     <td>${arrayParam[i].idNumber}</td>
-//     <td>${arrayParam[i].title}</td>
-//     <td>${arrayParam[i].annualSalary}</td>
-// </th>);
-
-
-// `
-//     <td>${arrayParam[i].firstName}</td>
-//     <td>${arrayParam[i].lastName}</td>
-//     <td>${arrayParam[i].idNumber}</td>
-//     <td>${arrayParam[i].title}</td>
-//     <td>${arrayParam[i].annualSalary}</td>
+// Table works, but functionality stops working when this code replaces <li>
+        // `<tr>
+        //     <td>${employeeArray[i].firstName}</td>
+        //     <td>${employeeArray[i].lastName}</td>
+        //     <td>${employeeArray[i].idNumber}</td>
+        //     <td>${employeeArray[i].title}</td>
+        //     <td>${employeeArray[i].annualSalary}</td> 
+        //     <td><button class="deleteBtn">delete</button></td>
+        // </td>`)
